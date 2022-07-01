04bb65eaf79d9312cb0610ba899f458a30093c69
Eric Crain <ericcrain77@gmail.com>
2022-06-30 22:46:06 -0400

    removing projectcard file to start from scratch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
-import Projects from './components/Projects/Projects';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Header />
       <Intro />
       <About />
-      <Projects />
     </>
     
   );
