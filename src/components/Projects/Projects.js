99d31b44a29bf0f01f4efdf020d0e8becdff3856
Eric Crain <ericcrain77@gmail.com>
2022-06-30 21:14:59 -0400

    adding project cards

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css';
+import ProjectCards from "./ProjectCards";
+
+function Projects() {
+    return (
+        <section className="portfolio">
+            <h2>Projects</h2>
+            <ProjectCards />
+        </section>
+    )
+}
+
+export default Projects;
