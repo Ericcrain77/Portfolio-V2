51dcfc9647dcc568c2ce73682b64d623e2a8356e
Eric Crain <ericcrain77@gmail.com>
2022-06-30 20:26:01 -0400

    adding about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,12 +4,16 @@ import './about.css';
 function About() {
     return (
         <div className="about-section">
-            <div>
-                <h2>About Me</h2>
-                <p>
-                    I am originally from Memphis, TN, but moved east to go to college at the University of Tennessee at Chattanooga, where I received a Bachelor of Science degree in 2015. The goal back then was to go to school to become a Physical Therapist. I worked at an orthopedic group, Center for Sports Medicine, here in Chattanooga in the Physical Therapy department as a Technician from 2014 to 2020. After tenaciously applying to Phyiscal Therapy School several times, I came to the conclusion that PT school was just not in the cards for me. I took on a new venture and now am currently the Shipping Manager of a wholesale plant nursery where I facilitate the shipping and receiving of various plants and plant products such as containers, ground cloth, fertilizer, etc. Although my path has lead me in other directions, I have always been interested in web development. In fact, it was one of my three choices when deciding my major in college, and looking back now I know it is what I was meant to choose all along.
-                </p>
-            </div>
+            <h2>A Little About Me</h2>
+            <p>
+                I am a Junior Web Developer near Chattanooga, Tennessee currently looking for a great fit to kickstart my career. I have worked in healthcare as well as the agriculture industry, so I bring a unique perspective and attitude to working on web applications. I am fueled by my passion for creating. Writing code to make functional components or applications, while also utilizing imaginitive design, gives me an incredible sense of fulfillment. 
+            </p>
+            <p>
+                I consider myself a 'forever student.' I am always willing and wanting to learn new technologies/frameworks/ways of doing things that I may not already know. I am dilligent, hard-working, and motivated to getting better each and every day.
+            </p>
+            <p>
+                I have base skills in HTML, CSS, JavaScript/TypeScript, MongoDB, Express.js, React.js, and Node.js (MERN Stack), with a goal of becoming an expert in Front-End Development.
+            </p>
         </div>
     )
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './header.css';
 import EC_icon_1024x1024 from '../../assets/icons/EC_icon_1024x1024.png';
 
 function Header() {
+    const [sticky, setSticky] = useState("");
+
+    useEffect(() => {
+        window.addEventListener('scroll', isSticky);
+        return () => {
+            window.removeEventListener('scroll', isSticky);
+        };
+    }, []);
+
+    const isSticky = () => {
+        const scrollTop = window.scrollY;
+        const stickyClass = scrollTop >= 250 ? "is-sticky" : "";
+        setSticky(stickyClass);
+        console.log(stickyClass);
+    };
+
+    const classes = `header-container ${sticky}`
+
     return (
-        <header className='header-container'>
+        <header className={classes}>
             <div>
                 <img src={EC_icon_1024x1024} alt='EC-Logo-Header' />
             </div>
