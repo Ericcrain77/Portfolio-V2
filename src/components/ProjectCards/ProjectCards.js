0bd5ac018fd18eda3b9683dcd38cd57748164ae7
Eric Crain <ericcrain77@gmail.com>
2022-07-03 16:16:03 -0400

    mapped over cards to make projects appear on the page.

--- src/components/ProjectCards/ProjectCards.js
+++ src/components/ProjectCards/ProjectCards.js
@@ -8,17 +8,23 @@ function ProjectCards() {
     return (
         <div className="project-section">
             <h1>Projects</h1>
-            <Card className="project-card">
-                <Card.Img variant="top" src={'../../assets/images/' + work[0].image + '.png'} />
-                <Card.Body>
-                    <Card.Title>{work[0].title}</Card.Title>
-                    <Card.Text>
-                        {work[0].description}
-                    </Card.Text>
-                    <i><a href={work[0].githubLink} target="_blank" rel="noopener noreferrer"><FaGithub /></a></i>
-                    <i><a href={work[0].appLink} target="_blank" rel="noopener noreferrer"><FaLink /></a></i>
-                </Card.Body>
-            </Card>
+            <div className="cards">
+                {
+                    work && work.length > 0 && work.map((work, i) =>
+                    <Card key={i} className="project-card">
+                        <Card.Img variant="top" src={require('../../assets/images/' + work.image + '.png')} className='card-image' />
+                        <Card.Body>
+                            <Card.Title>{work.title}</Card.Title>
+                            <Card.Text>
+                                {work.description}
+                            </Card.Text>
+                            <i><a href={work.githubLink} target="_blank" rel="noopener noreferrer"><FaGithub /></a></i>
+                            <i><a href={work.appLink} target="_blank" rel="noopener noreferrer"><FaLink /></a></i>
+                        </Card.Body>
+                    </Card>
+                    )
+                }
+            </div>
         </div>
     )
 };
