491fd4788c846730b65ab69bcfc49c76ea902c90
Eric Crain <ericcrain77@gmail.com>
2022-07-04 02:57:21 -0400

    adding styling to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,17 @@
 import React from "react";
 import './footer.css';
+import { IconContext } from 'react-icons';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
     return (
         <div className="footer-conatiner">
             <div>
-                <h3>Designed & Built by Eric Crain</h3>
+                <h3>Built and designed by Eric Crain</h3>
             </div>
             <div>
-                <i><a href="https://github.com/Ericcrain77" target="_blank" rel="noopener noreferrer"><FaGithub /></a></i>
-                <i><a href="https://www.linkedin.com/in/eric-crain-899a53218/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a></i>
+                <i><a href="https://github.com/Ericcrain77" target="_blank" rel="noopener noreferrer"><IconContext.Provider value={{className: 'footer-conatiner-icons', size: 32}}><FaGithub /></IconContext.Provider></a></i>
+                <i><a href="https://www.linkedin.com/in/eric-crain-899a53218/" target="_blank" rel="noopener noreferrer"><IconContext.Provider value={{className: 'footer-conatiner-icons', size: 32}}><FaLinkedin /></IconContext.Provider></a></i>
             </div>
         </div>
     )
