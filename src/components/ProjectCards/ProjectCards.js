c8da97962f4728f27ef286ed66c5f6d0aff428af
Eric Crain <ericcrain77@gmail.com>
2022-07-05 12:13:18 -0400

    styling the project cards

--- src/components/ProjectCards/ProjectCards.js
+++ src/components/ProjectCards/ProjectCards.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './projects.css';
 import work from './work.json';
+import { IconContext } from 'react-icons';
 import { FaGithub, FaLink } from 'react-icons/fa';
 import { Card } from 'react-bootstrap';
 
@@ -13,14 +14,16 @@ function ProjectCards() {
                     work && work.length > 0 && work.map((work, i) =>
                     <Card key={i} className="project-card">
                         <Card.Img variant="top" src={require('../../assets/images/' + work.image + '.png')} className='card-image' />
-                        <Card.Body>
-                            <Card.Title>{work.title}</Card.Title>
-                            <Card.Text>
+                        <Card.Body className="card-body">
+                            <Card.Title className="card-title">{work.title}</Card.Title>
+                            <Card.Text className="card-description">
                                 {work.description}
                             </Card.Text>
-                            <i><a href={work.githubLink} target="_blank" rel="noopener noreferrer"><FaGithub /></a></i>
-                            <i><a href={work.appLink} target="_blank" rel="noopener noreferrer"><FaLink /></a></i>
                         </Card.Body>
+                        <div className="card-links">
+                            <i><a href={work.githubLink} target="_blank" rel="noopener noreferrer"><IconContext.Provider value={{className: 'card-icons', size: 34}}><FaGithub /></IconContext.Provider></a></i>
+                            <i><a href={work.appLink} target="_blank" rel="noopener noreferrer"><IconContext.Provider value={{className: 'card-icons', size: 34}}><FaLink /></IconContext.Provider></a></i>
+                        </div>
                     </Card>
                     )
                 }
