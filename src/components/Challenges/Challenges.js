ec0856a82a83ac3f163dc4bfa2a43441c3fcc1eb
Eric Crain <ericcrain77@gmail.com>
2022-07-12 14:29:44 -0400

    trying to add fizzbuzz modal

--- src/components/Challenges/Challenges.js
+++ src/components/Challenges/Challenges.js
@@ -1,21 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from 'react-modal';
 import './challenges.css';
 
 Modal.setAppElement('#root');
 
 const MODAL_STYLES = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundcolor: 'var(--granite-gray)',
-    padding: '3rem',
-    zIndex: 1000,
+    content: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundcolor: 'var(--granite-gray)',
+        padding: '2rem',
+        boxShadow: '1px 1px 25px 5px black'
+    },
+    overlay: {
+        zIndex: 1000
+    }
 }
 
 function Challenges() {
     const [toggleModal, setToggleModal] = useState(false);
+    
+    const count = 1;
+
+    useEffect(() => {
+        if (toggleModal === true) {
+          document.body.style.overflow = 'hidden'
+        }
+        if (toggleModal === false) {
+            document.body.style.overflow = 'visible'
+        }
+    }, [toggleModal]);
+
+    const handleDecrement = () => {
+        if (count > 1) {
+            this.setState(
+                (prevState) => ({count: prevState.count - 1})
+            )
+        }
+    };
+
+    const handleIncrement = () => {
+        this.setState(
+            (prevState) => ({count: prevState.count + 1})
+        )
+    };
 
     return (
         <div className="challenges-section" id="coding-challenges">
@@ -31,9 +61,22 @@ function Challenges() {
                         isOpen={toggleModal} 
                         onRequestClose={() => setToggleModal(false)}
                     >
-                        <div className="challenge-modal">
-                            <h1>Hello</h1>
-                            <p>world!</p>
+                        <div className="challenge-modal-inside">
+                            <h1>React FizzBuzz</h1>
+                            <form>
+                                <input></input>
+                                <input></input>
+                            </form>
+                            <h2>
+                                {
+                                    (count % 15 === 0) ? "FizzBuzz" :
+                                    (count % 3 === 0)  ? "Fizz"     : 
+                                    (count % 5 === 0)  ? "Buzz"     : 
+                                    count
+                                }
+                            </h2>
+                            <button onClick={handleDecrement}>-</button>
+                            <button onClick={handleIncrement}>+</button>
                             <div>
                                 <button className='modal-btn' onClick={() => setToggleModal(false)}>Close</button>
                             </div> 
